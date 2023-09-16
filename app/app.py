014d72870f7b7504dc2095b3bb9df63014b8f296
urevoleg <urevolegg@gmail.com>
2023-09-16 20:47:26 +0300

    docker-compose

--- app/app.py
+++ app/app.py
@@ -1,5 +1,28 @@
+import os
+import datetime as dt
+
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_api import status
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+db = SQLAlchemy(app=app)
+
+
+@app.route('/')
+def index():
+    return jsonify({
+        'dt': dt.datetime.now(),
+        'db': {
+            'status': db.engine.connect().execute("""SELECT now();""").fetchone()._asdict()
+        }
+    }, status.HTTP_200_OK)
 
 
 if __name__ == '__main__':
-    while True:
-        pass
+    app.run(host='0.0.0.0', port=5000, debug=True)
