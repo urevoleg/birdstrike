325b1c6bfcd34917ab3bbc7f3713e1a7fd8ad9a5
urevoleg <urevolegg@gmail.com>
2023-09-21 22:24:12 +0300

    fix dumps

--- app/load.py
+++ app/load.py
@@ -115,7 +115,7 @@ if __name__ == '__main__':
                 near_row = sorted(response_data, key=lambda x: parse(x['DATE']) - row.raw_incidented_at)[0]
             else:
                 near_row = {}
-            output += [{**row._asdict(), 'json_data': json.dump(near_row, default=str)}]
+            output += [{**row._asdict(), 'json_data': json.dumps(near_row, default=str)}]
         pd.DataFrame(output).to_sql(name='weather_noaa', con=engine, if_exists='append', schema='raw')
         # break
 
